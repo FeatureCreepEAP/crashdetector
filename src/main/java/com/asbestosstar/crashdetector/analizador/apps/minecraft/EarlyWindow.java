bdde9463f7e8cd64ea43218cf5de5b9a90f82985
macbook <macbook@MBP-de-macbook>
2026-04-04 23:18:03 -0700

    Update EarlyWindow.java / %2 {1?}

--- src/main/java/com/asbestosstar/crashdetector/analizador/apps/minecraft/EarlyWindow.java
+++ src/main/java/com/asbestosstar/crashdetector/analizador/apps/minecraft/EarlyWindow.java
@@ -24,6 +24,11 @@ public class EarlyWindow implements Verificaciones {
 	private String enlaceHtml = "";
 	private boolean limitacionOpenGLMacOSDetectada = false;
 
+	
+	
+	//TODO Wayland: The platform does not provide the window position
+	
+	
 	/**
 	 * Verificación basada en el contenido completo de la consola.
 	 * <p>
